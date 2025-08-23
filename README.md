07f5670f3dcb0e313f10c8e463118352dfc4dfc5
Half_nothing <Half_nothing@163.com>
2025-08-23 16:14:46 +0800

    feat: 项目初始化

---
+++ README.md
@@ -0,0 +1,27 @@
+# FlyControlWeb
+
+一个用于模拟飞行的飞控前端  
+技术栈为 Vue + TypeScript + ElementPlus  
+配套后端请看[这个项目][Backend]
+
+---
+[![ReleaseCard]][Release]![ReleaseDataCard]  
+![LastCommitCard]![BuildStateCard]  
+![ProjectLanguageCard]![ProjectLicense]
+---
+
+[ReleaseCard]: https://img.shields.io/github/v/release/Flyleague-Collection/FlyControlWEB?style=for-the-badge&logo=github
+
+[ReleaseDataCard]: https://img.shields.io/github/release-date/Flyleague-Collection/FlyControlWEB?display_date=published_at&style=for-the-badge&logo=github
+
+[LastCommitCard]: https://img.shields.io/github/last-commit/Flyleague-Collection/FlyControlWEB?display_timestamp=committer&style=for-the-badge&logo=github
+
+[BuildStateCard]: https://img.shields.io/github/actions/workflow/status/Flyleague-Collection/FlyControlWEB/go-build.yml?style=for-the-badge&logo=github
+
+[ProjectLanguageCard]: https://img.shields.io/github/languages/top/Flyleague-Collection/FlyControlWEB?style=for-the-badge&logo=github
+
+[ProjectLicense]: https://img.shields.io/badge/License-MIT-blue?style=for-the-badge&logo=github
+
+[Release]: https://www.github.com/Flyleague-Collection/FlyControlWEB/releases/latest
+
+[Backend]: https://www.github.com/Flyleague-Collection/SimpleFSD
---
+++ vite.config.ts
@@ -0,0 +1,56 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {URL} from "node:url";
+import {fileURLToPath} from 'url'
+import {ElementPlusResolver} from "unplugin-vue-components/resolvers";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
+const pathSrc = fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig({
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()]
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()]
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': pathSrc
+        },
+        extensions: [
+            '.js',
+            '.json',
+            '.jsx',
+            '.mjs',
+            '.ts',
+            '.tsx',
+            '.vue'
+        ]
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: "modern-compiler"
+            }
+        }
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 8080,
+        proxy: {
+            "/api": "http://127.0.0.1"
+        }
+    },
+    esbuild: {
+        supported: {
+            "top-level-await": true
+        },
+        drop: [
+            // 'debugger'
+        ]
+    }
+})
